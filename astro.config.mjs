e40c9821cef66200a54773a4ae005dde2319ecc2
Nikhil B <59918974+heftymouse@users.noreply.github.com>
2023-07-24 12:45:16 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: { 
+			borderRadius: {
+				'control': '4px',
+				'surface': '8px'
+			},
+			colors: {
+				'background': 'var(--background)',
+				'text': 'CanvasText',
+				'accent-fill': 'var(--accentFill)',
+				'accent-text': 'var(--accentText)',
+				'card-fill': 'var(--cardFill)',
+				'card-stroke': 'var(--cardStroke)'
+			},
+			fontSize: {
+				'caption': ['0.85rem', '1rem'],
+				'body': ['1rem', '1.62rem'],
+				'h6': ['1.15rem', { lineHeight: '1.75rem', fontWeight: 600 }],
+				'h5': ['1.25rem', { lineHeight: '1.5rem', fontWeight: 600 }],
+				'h4': ['1.5rem', { lineHeight: '2rem', fontWeight: 600 }],
+				'h3': ['1.75rem', { lineHeight: '2.25rem', fontWeight: 600 }],
+				'h2': ['2rem', { lineHeight: '3.25rem', fontWeight: 600 }],
+				'h1': ['2.5rem', { lineHeight: '3.25rem', fontWeight: 600 }],
+			}
+		},
+	},
+	plugins: [],
+}
