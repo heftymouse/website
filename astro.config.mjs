7204eea300992835b647c1b2c4f6b18fe7041453
Nikhil B <59918974+heftymouse@users.noreply.github.com>
2023-08-13 17:30:24 +0530

    seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://heftymouse.me',
 	output: 'hybrid',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false
 		}),
-		mdx()
+		mdx(),
+    sitemap()
 	],
 	adapter: cloudflare(),
 	vite: {
