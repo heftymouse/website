a896dc66ac06f38fdd004d319afb26535ac42f57
Nikhil B <59918974+heftymouse@users.noreply.github.com>
2023-08-15 00:20:03 +0530

    e size large

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import {defineCollection, z} from 'astro:content';
 
 const blogCollection = defineCollection({
     type: 'content',
-    schema: ({image}) => z.object({
+    schema: () => z.object({
         title: z.string(),
         description: z.string().optional(),
         heroImage: z.string().optional()
