26073a76f0174c01c7913b33fc5352c3c2250cbc
Nikhil B <59918974+heftymouse@users.noreply.github.com>
2023-08-20 18:16:53 +0530

    adjust type ramp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,11 +22,11 @@ module.exports = {
 				caption: ['0.85rem', '1rem'],
 				body: ['1rem', '1.62rem'],
 				h6: ['1.15rem', { lineHeight: '1.75rem', fontWeight: 600 }],
-				h5: ['1.25rem', { lineHeight: '1.5rem', fontWeight: 600 }],
+				h5: ['1.35rem', { lineHeight: '1.5rem', fontWeight: 600 }],
 				h4: ['1.5rem', { lineHeight: '2rem', fontWeight: 600 }],
-				h3: ['1.75rem', { lineHeight: '2.25rem', fontWeight: 600 }],
-				h2: ['2rem', { lineHeight: '2.75rem', fontWeight: 600 }],
-				h1: ['2.5rem', { lineHeight: '3.25rem', fontWeight: 600 }]
+				h3: ['1.7rem', { lineHeight: '2.3rem', fontWeight: 600 }],
+				h2: ['1.9rem', { lineHeight: '2.65rem', fontWeight: 600 }],
+				h1: ['2.4rem', { lineHeight: '3.2rem', fontWeight: 600 }]
 			}
 		}
 	},
