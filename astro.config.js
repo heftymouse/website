bb355ad4b11aef4b07bfb8306a593c146836f28b
heftymouse <59918974+heftymouse@users.noreply.github.com>
2025-03-04 17:23:04 +0530

    tailwind 4

--- astro.config.js
+++ astro.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import { addTitleAndCaption } from './src/lib/markdown';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://heftymouse.me',
 	integrations: [
-		tailwind({
-			applyBaseStyles: false
-		}),
 		mdx(),
 		sitemap()
 	],
@@ -29,6 +26,9 @@ export default defineConfig({
 				}
 			}
 		},
+		plugins: [
+			tailwindcss()
+		],
 		ssr: {
 			external: ['node:fs/promises', 'node:path', '@node-rs/xxhash'],
 		}
